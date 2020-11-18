ab8db041edd91e6360650e2712097fff0bdab88d
many <maxime@meilisearch.com>
2020-11-18 18:01:50 +0100

    adapt to have original byte_indices

--- src/internal_tokenizer/jieba.rs
+++ src/internal_tokenizer/jieba.rs
@@ -1,53 +1,54 @@
 use std::borrow::Cow;
 use crate::{Token, TokenKind};
-use super::InternalTokenizer;
+use crate::processors::ProcessedText;
+use super::{InternalTokenizer, TokenStream};
 use jieba_rs::Jieba as JiebaTokenizer;
-use jieba_rs::Token as JiebaToken;
 // use deunicode::deunicode;
 
 pub struct Jieba {
     jieba: JiebaTokenizer,
 }
 
-impl<'a> InternalTokenizer<'a> for Jieba {
-    type Output = JiebaIterator<'a>;
-    fn tokenize(&self, s: &'a str) -> Self::Output {
-        let tokenized = self.jieba.tokenize(s, jieba_rs::TokenizeMode::Default, false);
+impl InternalTokenizer for Jieba {
+    fn tokenize<'a>(&self, s: &'a ProcessedText<'a>) -> TokenStream<'a> {
+        let tokenized = self.jieba.tokenize(&s.processed, jieba_rs::TokenizeMode::Default, false);
 
-        JiebaIterator(
-            Box::new(tokenized.into_iter().scan(0, |state, jieba_token| {
-                let old_state = *state;
-                *state = *state + jieba_token.word.as_bytes().len();
+        let original_byte_len = s.original.len();
+        let mut original = s.original.char_indices()
+            // map only byte indices
+            .map(|(byte_index, _)| byte_index)
+            // add ending byte index
+            .chain(std::iter::once(original_byte_len));
+
+        TokenStream {
+            inner: Box::new(tokenized.into_iter().scan(0, move |byte_index, jieba_token| {
+                let char_start = jieba_token.start;
+                let char_end = jieba_token.end;
+                let byte_start = *byte_index;
+
+                // iter.nth(0) == iter.next(), so nth is computed as `char_end - char_start - 1`
+                // but not for the first iteration where nth is computed as `char_end`
+                let byte_end = match *byte_index {
+                    // TODO: investgate if unwrap can fail
+                    0 => original.nth(char_end).unwrap(),
+                    _ => original.nth(char_end - char_start - 1).unwrap(),
+                };
+
+                *byte_index = byte_end;
 
                 Some(Token {
                     kind: TokenKind::Word,
                     word: Cow::Borrowed(jieba_token.word),
-                    byte_start: old_state,
-                    byte_end: *state,
+                    byte_start,
+                    byte_end,
                 })
             }))
-        )
+        }
     }
 }
 
 impl Default for Jieba {
     fn default() -> Self { Jieba { jieba: JiebaTokenizer::new() } }
-}
-
-pub struct JiebaIterator<'a>(Box<dyn Iterator<Item = Token<'a>> + 'a>);
-
-impl<'a> JiebaIterator<'a> {
-    fn normalize(s: &'a str) -> Cow<str> {
-        s.into()
-    }
-}
-
-impl<'a> Iterator for JiebaIterator<'a> {
-    type Item = Token<'a>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.0.next()
-    }
 }
 
 
@@ -59,13 +60,21 @@ mod test {
     fn test_simple() {
         let tokenizer = Jieba::default();
         let orig = "The quick (\"brown\") fox can't jump 32.3 feet, right? Brr, it's 29.3°F!";
-        let tokens = tokenizer.tokenize(orig);
+        let processed = ProcessedText {
+            original: orig,
+            processed: Cow::Borrowed(orig),
+        };
+        let tokens = tokenizer.tokenize(&processed);
         assert_eq!(orig, tokens.map(|t| &orig[t.byte_start..t.byte_end]).collect::<String>());
         
         let orig = "為一包含一千多萬目詞的帶標記平衡語料庫";
-        let tokens = tokenizer.tokenize(orig);
+        let processed = ProcessedText {
+            original: orig,
+            processed: Cow::Borrowed(orig),
+        };
+        let tokens = tokenizer.tokenize(&processed);
         assert_eq!(orig, tokens.map(|t| &orig[t.byte_start..t.byte_end]).collect::<String>());
-        let tokens = tokenizer.tokenize(orig);
+        let tokens = tokenizer.tokenize(&processed);
         println!("{:#?}", tokens.collect::<Vec<_>>());
     }
 }
