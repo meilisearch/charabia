101c9f7ffb7115fd68aa2a97909668eaea34ce8b
mpostma <postma.marin@protonmail.com>
2020-11-24 17:19:34 +0100

    document eraser

--- src/processors/eraser.rs
+++ src/processors/eraser.rs
@@ -5,6 +5,9 @@ use super::{ProcessedText, PreProcessor};
 pub struct Eraser(char, String);
 
 impl Eraser {
+    /// replaces the occurences of `c`, if any, with the equivalent amount of spaces in bytes.
+    /// e.g: "l’espagne" becomes "l   espagne" because '’' is 3 bytes long. This allows the string
+    /// to remains the same size as the original, an preserve the offsets of the tokens
     pub fn new(c: char) -> Self {
         Self(c, std::iter::repeat(' ').take(c.len_utf8()).collect())
     }
