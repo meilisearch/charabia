aa64ccf0bcdae2cad582ee879adceb85065c0993
many <maxime@meilisearch.com>
2020-11-24 17:12:33 +0100

    add pre_processor in chinese pipeline

--- src/processors/chinese_translation.rs
+++ src/processors/chinese_translation.rs
@@ -18,3 +18,31 @@ impl PreProcessor for ChineseTranslationPreProcessor {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_traditional_to_simplified() {
+        let preprocessor = ChineseTranslationPreProcessor;
+
+        let traditional = "人人生而自由﹐在尊嚴和權利上一律平等。他們賦有理性和良心﹐並應以兄弟關係的精神互相對待。";
+        let simplified = "人人生而自由﹐在尊严和权利上一律平等。他们赋有理性和良心﹐并应以兄弟关系的精神互相对待。";
+
+        let analyzed = preprocessor.process(&traditional);
+
+        assert_eq!(simplified, &analyzed.processed);
+    }
+
+    #[test]
+    fn test_simplified_to_simplified() {
+        let preprocessor = ChineseTranslationPreProcessor;
+
+        let simplified = "人人生而自由﹐在尊严和权利上一律平等。他们赋有理性和良心﹐并应以兄弟关系的精神互相对待。";
+
+        let analyzed = preprocessor.process(&simplified);
+
+        assert_eq!(simplified, &analyzed.processed);
+    }
+}
