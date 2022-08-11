5ca8f36d77906cdaf613f8302340224c0e118a97
aFluffyHotdog <93137058+aFluffyHotdog@users.noreply.github.com>
2022-08-11 14:56:36 +0700

    1st full implementation + cleaned up unused stuff

--- benches/bench.rs
+++ benches/bench.rs
@@ -11,14 +11,15 @@ static DATA_SET: &[((usize, Script, Language), &str)] = &[
     ((132, Script::Latin, Language::Eng), "The quick (\"brown\") fox can't jump 32.3 feet, right? Brr, it's 29.3°F! Hello guys, my purpose is to benchmark tokenizer properly."),
     ((132, Script::Latin, Language::Fra), "La ville avait d'abord été nommée « Lutèce » ou « boueuse », ici une tentative d'explication par le latin lŭtum « boue »."),
     ((132, Script::Hebrew, Language::Heb), "הַשּׁוּעָל הַמָּהִיר (״הַחוּם״) לֹא יָכוֹל לִקְפֹּץ 8.94 מֶטְרִים, נָכוֹן?"),
-
+    ((132, Script::Thai, Language::Tha), "ไก่จิกเด็กตายเด็กตายบนปากโอ่งไก่อะไรวะโหดจัง"),
     // long texts (~365 bytes)
     ((363, Script::Cj, Language::Cmn), "距今60万年-2万年的时间内，北京地区处于旧石器时代，在周口店发现了旧石器时代早期北京直立人、中期新洞人和晚期山顶洞人的典型遗址。北京地区在不晚于1万年前已经开始进入新石器时代。当时该地区人类定居生活固定化，逐渐从山洞中迁徙出来，到平原地区定居[12]。"),
     ((364, Script::Cj, Language::Jpn), "詳しくは以下の をご覧下さい。語学ないし文学の立場からの価値判断は一切おこなっていません。だけど、バラ科の仲間ということでは「すもももももももものうち」は正しいことになります。すももものうち！今日は「すもももももももものうち」について考えます。"),
     ((363, Script::Latin, Language::Eng), "The City of London Corporation is unique in the UK and has some unusual responsibilities for a local council, such as being the police authority. It is also unusual in having responsibilities and ownership beyond its boundaries. The Corporation is headed by the Lord Mayor of the City of London (an office separate from, and much older than, the Mayor of London)."),
     ((363, Script::Latin, Language::Fra), "La position de Lutèce, sur l'île aujourd'hui nommée l'île de la Cité, permettant le franchissement du grand fleuve navigable qu'est la Seine par une voie reliant le Nord et le Sud des Gaules, en fait dès l'Antiquité une cité importante, capitale des Parisii, puis lieu de séjour d'un empereur romain. Le mot Lutèce resulte de la francisation de Lutetia."),
     ((365, Script::Hebrew, Language::Heb), "הַשּׁוּעָל הַמָּהִיר (״הַחוּם״) לֹא יָכוֹל לִקְפֹּץ 8.94 מֶטְרִים, נָכוֹן? תַּכְלֶס, אִם הוּא הָיָה יָכוֹל, הוּא חֲתִיכַת שׁוּעָל הַשּׁוּעָל הַזֶּה.. אֲבָל הַאִם לֹא כֻּלָּנוּ שׁוּעָלִים בְּעֶצֶם? יתכן."),
-];
+    ((132, Script::Thai, Language::Tha), "เราจะทำตามสัญญาขอเวลาอีกไม่นานแล้วแผ่นดินที่งดงามจะคืนกลับมาเราจะทำอย่างซื่อตรงขอแค่เธอจงไว้ใจและศรัทธาแผ่นดินจะดีในไม่ช้า"),
+    ];
 
 criterion_group!(benches, criterion_benchmark);
 criterion_main!(benches);
--- src/segmenter/thai.rs
+++ src/segmenter/thai.rs
@@ -4,17 +4,10 @@ use tokenizer::{Tokenizer, th};
 use once_cell::sync::Lazy;
 
 
-/*
-Make a small documentation of the specialized Segmenter like below.
-*/
-/// <Script/Language> specialized [`Segmenter`].
+
+/// Thai specialized [`Segmenter`].
 ///
-/// This Segmenter uses [`<UsedLibraryToSegment>`] internally to segment the provided text.
-/// <OptionalAdditionnalExplanations>
-//
-//TIP: Name the Segmenter with its purpose and not its internal behavior:
-//     prefer JapaneseSegmenter (based on the Language) instead of LinderaSegmenter (based on the used Library).
-//     Same for the filename, prefer `japanese.rs` instead of `lindera.rs`.
+/// This Segmenter uses the very creatively named, tokenizer library internally to segment the provided text.
 
 pub struct ThaiSegmenter;
 static SOMCHAI: Lazy<tokenizer::th::Tokenizer> = Lazy::new(||
@@ -24,57 +17,28 @@ static SOMCHAI: Lazy<tokenizer::th::Tokenizer> = Lazy::new(||
     tokenizer
 });
 
-/*
-All specialized segmenters only need to implement the method `segment_str` of the `Segmenter` trait.
-*/
+
 impl Segmenter for ThaiSegmenter {
     fn segment_str<'o>(&self, to_segment: &'o str) -> Box<dyn Iterator<Item = &'o str> + 'o> {
-        // Create the iterator that will segment the provided text.
-        // Assuming dictinoary contains "ภาษาไทย" and "นิดเดียว" but not "ง่าย"
-        //let somchai = th::Tokenizer::new("C:\\Users\\macth\\Desktop\\Rust-Playground\\src\\words_th.txt").expect("Dictionary file not found");
-    
         let segmented = SOMCHAI.tokenize(to_segment);
-
-        // Return the created iterator wrapping it in a Box.
         Box::new(segmented.into_iter())
     }
 }
 
-
-//TIP: Some segmentation Libraries need to initialize a instance of the Segmenter.
-//     This initialization could be time-consuming and shouldn't be done at each call of `segment_str`.
-//     In this case, you may want to store the initialized instance in a lazy static like below and call it in `segment_str`.
-//     Otherwise, just remove below lines.
-//
-// Put this import at the top of the file.
-// use once_cell::sync::Lazy;
-//
-// static LIBRARY_SEGMENTER: Lazy<LibrarySegmenter> = Lazy::new(|| LibrarySegmenter::new());
-
-// Publish the newly implemented Segmenter:
-//	   - import module by adding `mod dummy;` (filename) in `segmenter/mod.rs`
-//	   - publish Segmenter by adding `pub use dummy::DummySegmenter;` in `segmenter/mod.rs`
-//     - running `cargo doc --open` you should see your Segmenter in the segmenter module
 
 // Test the segmenter:
 #[cfg(test)]
 mod test {
     use crate::segmenter::test::test_segmenter;
 
-    // Original version of the text.
+    
     const TEXT: &str = "ภาษาไทยง่ายนิดเดียว";
 
-    // Segmented version of the text.
     const SEGMENTED: &[&str] = &["ภาษาไทย", "ง่าย", "นิดเดียว"];
 
-    // Segmented and normalized version of the text.
+
     const TOKENIZED: &[&str] = SEGMENTED;
     // Macro that run several tests on the Segmenter.
     test_segmenter!(ThaiSegmenter, TEXT, SEGMENTED, TOKENIZED, Script::Thai, Language::Tha);
 }
 
-// Include the newly implemented Segmenter in the tokenization pipeline:
-//	   - assign Segmenter to a Script and a Language by adding it in `SEGMENTERS` in `segmenter/mod.rs`
-//	   - check if it didn't break any test or benhchmark
-
-// Your Segmenter will now be used on texts of the assigned Script and Language. Thank you for your contribution, and congratulation! 🎉
