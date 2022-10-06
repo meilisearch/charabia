8fc3255a42864b51cf2495b5766c1a9d3885d485
Jan Vandepitte <yenwel1987@gmail.com>
2022-10-06 14:23:05 +0200

    implement segment methods with allowlist

--- src/detection/mod.rs
+++ src/detection/mod.rs
@@ -11,12 +11,12 @@ pub struct StrDetection<'a> {
     inner: &'a str,
     pub script: Option<Script>,
     pub language: Option<Language>,
-    allow_lists_map : Option<HashMap<Script,Vec<Language>>>,
+    allow_list : &'a Option<HashMap<Script,Vec<Language>>>,
 }
 
 impl<'a> StrDetection<'a> {
-    pub fn new(inner: &'a str, allow_lists_map: Option<HashMap<Script,Vec<Language>>>) -> Self {
-        Self { inner, script: None, language: None, allow_lists_map }
+    pub fn new(inner: &'a str, allow_list: &'a Option<HashMap<Script,Vec<Language>>>) -> Self {
+        Self { inner, script: None, language: None, allow_list: &allow_list }
     }
 
     pub fn script(&mut self) -> Script {
@@ -26,7 +26,7 @@ impl<'a> StrDetection<'a> {
 
     pub fn language(&mut self) -> Language {
         let inner = self.inner;
-        *self.language.get_or_insert_with(|| Self::detect_lang(inner, self.script, &self.allow_lists_map))
+        *self.language.get_or_insert_with(|| Self::detect_lang(inner, self.script, self.allow_list))
     }
 
     /// detect script with whatlang,
@@ -37,10 +37,10 @@ impl<'a> StrDetection<'a> {
 
     /// detect lang with whatlang
     /// if no language is detected, return Language::Other
-    fn detect_lang(text: &str, script: Option<Script>, allow_lists_map : &Option<HashMap<Script,Vec<Language>>>) -> Language {
+    fn detect_lang(text: &str, script: Option<Script>, allow_list : &Option<HashMap<Script,Vec<Language>>>) -> Language {
         if let Some(script) = script {
-            if let Some(allow_lists_map) = allow_lists_map {
-                if let Some(allow_list) = allow_lists_map.get(&script){
+            if let Some(allow_list) = allow_list {
+                if let Some(allow_list) = allow_list.get(&script){
                     if !allow_list.is_empty() {
                         let allow_list = allow_list.iter().map(|lang| (*lang).into()).collect();
                         let detector = Detector::with_allowlist(allow_list);
@@ -53,13 +53,13 @@ impl<'a> StrDetection<'a> {
     }
 }
 
-pub trait Detect {
-    fn detect(&self, allow_lists_map: Option<HashMap<Script,Vec<Language>>>) -> StrDetection;
+pub trait Detect<'a> {
+    fn detect(&'a self, allow_list: &'a Option<HashMap<Script,Vec<Language>>>) -> StrDetection<'a>;
 }
 
-impl Detect for &str {
-    fn detect(&self, allow_lists_map: Option<HashMap<Script,Vec<Language>>>) -> StrDetection 
+impl<'a> Detect<'a> for &str {
+    fn detect(&'a self, allow_list: &'a Option<HashMap<Script,Vec<Language>>>) -> StrDetection<'a> 
     {
-        StrDetection::new(self,allow_lists_map)
+        StrDetection::new(self,&allow_list)
     }
 }
