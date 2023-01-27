93b8ec953de45debaa3c563b2e4f69f2e430e5c1
Lawrence Chou <choznerol@protonmail.com>
2023-01-27 16:37:10 +0800

    Use absolute path for kVariant.(c|t)sv
    https://github.com/meilisearch/charabia/pull/171#discussion_r1072409751
    
    Co-authored-by: Many the fish <many@meilisearch.com>

--- kvariants/build.rs
+++ kvariants/build.rs
@@ -1,5 +1,5 @@
 use serde::Deserialize;
-use std::error::Error;
+use std::{env, error::Error, path::Path};
 
 #[derive(Deserialize)]
 pub struct TsvRow {
@@ -18,14 +18,15 @@ fn main() {
 }
 
 fn compress_kvariant_txt() -> Result<(), Box<dyn Error>> {
-    let reader = csv::ReaderBuilder::new()
-        .delimiter(b'\t')
-        .has_headers(false)
-        .from_path("dictionaries/source/kVariants.tsv");
+    let manifest_dir = env::var_os("CARGO_MANIFEST_DIR").unwrap();
+    let src_path = Path::new(&manifest_dir).join("dictionaries/source/kVariants.tsv");
+    let mut reader =
+        csv::ReaderBuilder::new().delimiter(b'\t').has_headers(false).from_path(src_path)?;
 
-    let mut writer = csv::Writer::from_path("dictionaries/compressed/kVariants.csv")?;
+    let dest_path = Path::new(&manifest_dir).join("dictionaries/compressed/kVariants.csv");
+    let mut writer = csv::Writer::from_path(dest_path)?;
 
-    for result in reader?.deserialize() {
+    for result in reader.deserialize() {
         let line: TsvRow = result?;
 
         // Extract "㨲" from "㨲 (U+3A32)"
