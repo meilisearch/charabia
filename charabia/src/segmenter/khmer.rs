91049d6f254bac63ae6121064a6c1f9469dfc877
xshadowlegendx <lyheng0@gmail.com>
2023-03-29 11:35:36 +0700

    adding khmer segmenter

--- charabia/src/segmenter/khmer.rs
+++ charabia/src/segmenter/khmer.rs
@@ -1,5 +1,31 @@
+use std::vec;
+
 // Import `Segmenter` trait.
 use crate::segmenter::Segmenter;
+use icu::segmenter::{WordSegmenter};
+
+extern crate alloc; // required as my-data-mod is written for #[no_std]
+use icu::locid::{locale, Locale};
+use icu_provider_blob::BlobDataProvider;
+
+//TIP: Some segmentation Libraries need to initialize a instance of the Segmenter.
+//     This initialization could be time-consuming and shouldn't be done at each call of `segment_str`.
+//     In this case, you may want to store the initialized instance in a lazy static like below and call it in `segment_str`.
+//     Otherwise, just remove below lines.
+//
+// Put this import at the top of the file.
+use once_cell::sync::Lazy;
+//
+static SEGMENTER: Lazy<WordSegmenter> = Lazy::new(|| {
+    const LOCALE: Locale = locale!("km");
+
+    let blob: Vec<u8> = std::fs::read("./icu4x-khmer-keys").expect("failed to read khmer keys");
+
+    let buffer_provider: BlobDataProvider = 
+        BlobDataProvider::try_new_from_blob(blob.into_boxed_slice()).expect("failed to load khmer keys");
+
+    WordSegmenter::try_new_with_buffer_provider(&buffer_provider).expect("failed to initialize khmer word segmenter")
+});
 
 // Make a small documentation of the specialized Segmenter like below.
 /// <Script/Language> specialized [`Segmenter`].
@@ -15,23 +41,27 @@ pub struct KhmerSegmenter;
 // All specialized segmenters only need to implement the method `segment_str` of the `Segmenter` trait.
 impl Segmenter for KhmerSegmenter {
     fn segment_str<'o>(&self, to_segment: &'o str) -> Box<dyn Iterator<Item = &'o str> + 'o> {
-        // Create the iterator that will segment the provided text.
-        let segment_iterator = to_segment.split_inclusive(' ');
+        let (_, positions) =
+            SEGMENTER
+            .segment_str(to_segment)
+            .fold((None, vec![]), |mut acc, elem| {
+                if acc.0.is_some() {
+                    acc.1.push((acc.0.unwrap(), elem));
+                }
+
+                acc.0 = Some(elem);
+
+                acc
+            });
 
         // Return the created iterator wrapping it in a Box.
-        Box::new(segment_iterator)
+        Box::new(
+            positions.iter().map(|(start, end)|
+                &to_segment[*start..*end]
+            ).collect::<Vec<&str>>().into_iter()
+        )
     }
 }
-
-//TIP: Some segmentation Libraries need to initialize a instance of the Segmenter.
-//     This initialization could be time-consuming and shouldn't be done at each call of `segment_str`.
-//     In this case, you may want to store the initialized instance in a lazy static like below and call it in `segment_str`.
-//     Otherwise, just remove below lines.
-//
-// Put this import at the top of the file.
-// use once_cell::sync::Lazy;
-//
-// static LIBRARY_SEGMENTER: Lazy<LibrarySegmenter> = Lazy::new(|| LibrarySegmenter::new());
 
 // Publish the newly implemented Segmenter:
 //	   - import module by adding `mod dummy;` (filename) in `segmenter/mod.rs`
@@ -44,16 +74,16 @@ mod test {
     use crate::segmenter::test::test_segmenter;
 
     // Original version of the text.
-    const TEXT: &str = "Hello World!";
+    const TEXT: &str = "សួស្តីពិភពលោក";
 
     // Segmented version of the text.
-    const SEGMENTED: &[&str] = &["Hello", " World!"];
+    const SEGMENTED: &[&str] = &["សួស្តី", "ពិភពលោក"];
 
     // Segmented and normalized version of the text.
-    const TOKENIZED: &[&str] = &["hello", " world!"];
+    const TOKENIZED: &[&str] = &["សួស្តី", "ពិភពលោក"];
 
     // Macro that run several tests on the Segmenter.
-    test_segmenter!(KhmerSegmenter, TEXT, SEGMENTED, TOKENIZED, Script::Latin, Language::Other);
+    test_segmenter!(KhmerSegmenter, TEXT, SEGMENTED, TOKENIZED, Script::Khmer, Language::Khm);
 }
 
 // Include the newly implemented Segmenter in the tokenization pipeline:
