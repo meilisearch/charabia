9fd93509a8ccb401d4e517cbdfa624afc694a757
ManyTheFish <many@meilisearch.com>
2025-04-09 11:34:10 +0200

    Add Llama comparison

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,6 @@
 use charabia::{Language, TokenizerBuilder};
 use clap::{Parser, ValueEnum};
+use tokenizers::Tokenizer;
 
 #[derive(Parser)]
 struct Args {
@@ -18,19 +19,31 @@ enum Command {
 fn main() {
     let args = Args::parse();
     let tokenizer = TokenizerBuilder::default().into_tokenizer();
+    let text = args.text.as_str();
 
     let language = args.language.and_then(|l| Language::from_code(&l)).map(|l| vec![l]);
     let language = language.as_ref().map(|l| l.as_slice());
     let tokens = match args.command {
         Command::Tokenize => tokenizer
-            .tokenize_with_allow_list(args.text.as_str(), language)
+            .tokenize_with_allow_list(text, language)
             .map(|l| l.lemma().to_owned())
             .collect::<Vec<_>>(),
         Command::Segment => tokenizer
-            .segment_with_allow_list(args.text.as_str(), language)
+            .segment_with_allow_list(text, language)
             .map(|l| l.lemma().to_owned())
             .collect::<Vec<_>>(),
     };
 
-    println!("{:?}", tokens);
+    println!("Charabia: {:?}", tokens);
+    println!(
+        "Llama: {:?}",
+        Tokenizer::from_pretrained("Xenova/llama4-tokenizer", None)
+            .expect("Failed to load LLaMA tokenizer")
+            .encode(text, false)
+            .expect("Failed to encode text")
+            .get_offsets()
+            .iter()
+            .map(|(start, end)| (&text[*start..*end]).to_owned())
+            .collect::<Vec<_>>()
+    );
 }
